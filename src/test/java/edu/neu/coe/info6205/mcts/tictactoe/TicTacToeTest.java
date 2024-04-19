55dbe5dcc3561c179e7cd427dab55cfcced3c71b
Saurabh Srivastava <srivastava.sau@northeastern.edu>
2024-04-18 20:34:28 -0400

    Updating MCTS

--- src/test/java/edu/neu/coe/info6205/mcts/tictactoe/TicTacToeTest.java
+++ src/test/java/edu/neu/coe/info6205/mcts/tictactoe/TicTacToeTest.java
@@ -15,11 +15,27 @@ public class TicTacToeTest {
     @Test
     public void runGame() {
         long seed = 0L;
-        TicTacToe target = new TicTacToe(seed); // games run here will all be deterministic.
-        State<TicTacToe> state = target.runGame();
-        Optional<Integer> winner = state.winner();
-        if (winner.isPresent()) assertEquals(Integer.valueOf(TicTacToe.X), winner.get());
-        else fail("no winner");
+        TicTacToe target = new TicTacToe(seed);
+        int mctsWon = 0;
+        int randomWon = 0;
+        int draw = 0;
+        for(int i = 0; i < 100; i++){
+            State<TicTacToe> state = target.runGame();
+            Optional<Integer> winner = state.winner();
+            if(winner.isPresent()){
+                if(winner.get() == Integer.valueOf(TicTacToe.X)) mctsWon +=1;
+                else if (winner.get() == Integer.valueOf(TicTacToe.O)) randomWon += 1;
+            }
+
+            else draw +=1;
+        }
+        System.out.println("MCTS won " + mctsWon);
+        System.out.println("Random won " + randomWon);
+        System.out.println("Draw " + draw);
+
+
+//        if (winner.isPresent()) assertEquals(Integer.valueOf(TicTacToe.O), winner.get());
+//        else fail("no winner");
     }
     @Test
     public void testPlayerAlternation() {
